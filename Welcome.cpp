2344bc070a4a166aa6ea8f5fced9c50ac420730c
ksshah <ksshah55@gmail.com>
2015-08-28 08:33:29 +0530

    Update Welcome.cpp

--- Welcome.cpp
+++ Welcome.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main()
 {
   cout<<"Hello charu.Welcome to A3 services.\n";
-  cout<<"Hope you are doing fine in the organization\n"
+  cout<<"Hope you are doing fine in the organization\n";
+  cout<<"Did you see the message?";
   return 0;
 }
